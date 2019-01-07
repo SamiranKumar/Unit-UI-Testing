b248cd01fcc353d9049dfbe2284371b1fbabb19e
SamiranKumar <samirankumar11@gmail.com>
2019-01-07 17:22:14 +0600

    First Unit test

---
+++ app/src/main/java/com/skh/shomu/android_testing/util/FirstNameExtractor.java
@@ -0,0 +1,9 @@
+package com.skh.shomu.android_testing.util;
+
+public class FirstNameExtractor {
+
+    public static String extractFirstName(String fullName) {
+        return fullName;
+    }
+
+}
---
+++ app/src/test/java/com/skh/shomu/android_testing/util/FirstNameExtractorTest.java
@@ -0,0 +1,57 @@
+package com.skh.shomu.android_testing.util;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static com.skh.shomu.android_testing.util.FirstNameExtractor.extractFirstName;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.*;
+
+public class FirstNameExtractorTest {
+
+    private static String FULL_NAME_DEMO = "Samiran Kumar";
+
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+
+    @Test
+    public void extractFirstName_EmptyString_ReturnEmptyString() {
+        assertThat(extractFirstName(""), is(""));
+    }
+
+
+    @Test
+    public void extractFirstName_DifferentString_ReturnEmptyString() {
+        assertThat(extractFirstName("Samiran"), is(FULL_NAME_DEMO));
+        assertThat(extractFirstName("Samiran Kumar"), is(FULL_NAME_DEMO));
+
+    }
+
+
+    @Test
+    public void extractFirstName_WithSpaceDifferentString_ReturnEmptyString() {
+        assertThat(extractFirstName("Samiran "), is(FULL_NAME_DEMO));
+        assertThat(extractFirstName(" Samiran"), is(FULL_NAME_DEMO));
+        assertThat(extractFirstName(" Samiran "), is(FULL_NAME_DEMO));
+        assertThat(extractFirstName("Samiran  Kumar"), is(FULL_NAME_DEMO));
+        assertThat(extractFirstName(" Samiran  Kumar"), is(FULL_NAME_DEMO));
+        assertThat(extractFirstName(" Samiran  Kumar "), is(FULL_NAME_DEMO));
+
+    }
+
+
+    @Test
+    public void extractFirstName_FullNameString_ReturnEmptyString() {
+        assertThat(extractFirstName("Samiran Kumar"), is(FULL_NAME_DEMO));
+    }
+
+
+}
