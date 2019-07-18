46889816da91ba3a73cd31a835c7221787e04e47
samadtalukder <samadtalukdar6@gmail.com>
2019-07-18 15:31:58 +0600

    simple email validator class create

---
+++ app/src/test/java/com/skh/shomu/android_testing/util/EmailValidatorTest.java
@@ -0,0 +1,20 @@
+package com.skh.shomu.android_testing.util;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class EmailValidatorTest {
+    private static String SAMPLE_EMAIL_DEMO = "samad@gmail.com";
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void emailValidator_CorrectEmailSample_ReturnsTrue() {
+        
+    }
+}
